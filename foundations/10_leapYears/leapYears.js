05e38fd896e8d823c754c7dc08affc7830602a4f
Carter Edginton <249395916+carter-edg@users.noreply.github.com>
2026-01-29 18:57:45 -0700

    Solution for 10_leapYears
    My solution was almost exactly like the solution's, but my boolean logic
    is different with the same result.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isYearDivisibleByFour = year % 4 === 0
+    let isYearCentury = year % 100 === 0
+    let isYearDivisibleByFourHundredHundred = year % 400 === 0
+    return (isYearDivisibleByFourHundredHundred || (!isYearCentury && isYearDivisibleByFour));
 };
 
 // Do not edit below this line
