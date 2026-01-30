77ef0bab1b5ce2ae41f5cf1c3b53a1b81e1d13d0
Carter Edginton <249395916+carter-edg@users.noreply.github.com>
2026-01-29 18:46:30 -0700

    Solution for 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0){
+        return 'ERROR';
+    }
+    if (!Number.isInteger(a) || !Number.isInteger(b)){
+        return 'ERROR';
+    }
+    let smallNum = a;
+    let largeNum = b;
+    if (smallNum === largeNum) {return smallNum;}
+    if (b < a){
+        smallNum = b;
+        largeNum = a;
+    }
+    let sum = 0;
+    for (let i = smallNum; i <= largeNum; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
